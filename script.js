57a0c87863beadb254a6bd65f445be2a8c508bda
abdu <kubanycuuluabdurahim805@gmail.co>
2022-05-17 19:25:43 +0600

    firsta

---
+++ script.js
@@ -0,0 +1,54 @@
+// let person ={
+//     name: "navi" ,
+//     age:20,
+//     big :{
+//     shop: "big shop",
+//     lest: "git"
+
+//     }
+// }
+// console.log(Object.keys(person)) 
+// console.log(Object.values(person.big))
+// document.write(person)
+// alert(person)
+
+
+
+
+
+
+
+// Функция
+
+// function big( a, b, k){
+//     console.log(a+b*k)
+
+// }
+// big(10,45,25)
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+// let buton = document.getElementById("but")
+// buton.addEventListener('click' ,func)
+// function func(){
+//     alert("you  hello ")
+// }
+
+let buton = document.querySelector(".gui")
+buton.addEventListener('click' ,func)
+let p =  document.getElementById("p")
+function func(){
+    p.style.fontSize =}
+    
